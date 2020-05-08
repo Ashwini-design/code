1a20d88ec7c7683e5d90a06445fe3e1b2bb79815
Ashwini-design <65044871+Ashwini-design@users.noreply.github.com>
2020-05-08 17:07:26 -0400

    First Commit

---
+++ Aoc2016/src/d10/Utils.java
@@ -0,0 +1,53 @@
+package d10;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.List;
+import java.util.stream.IntStream;
+
+import static java.util.stream.Collectors.toList;
+
+public class Utils {
+
+    public static String md5(final String input) {
+        try {
+            MessageDigest md = MessageDigest.getInstance("MD5");
+            md.update(input.getBytes());
+            byte[] digest = md.digest();
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < digest.length; i++) {
+                String hex = Integer.toHexString(0xFF & digest[i]);
+                if (hex.length() == 1) {
+                    sb.append('0');
+                }
+                sb.append(hex);
+            }
+            return sb.toString();
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static List<String> zip(List<String> strings) {
+        int length = strings.get(0).length();
+        return IntStream
+                .range(0, length)
+                .mapToObj(i -> {
+                    StringBuilder sb = new StringBuilder();
+                    for (String s : strings) {
+                        sb.append(s.charAt(i));
+                    }
+                    return sb.toString();
+                })
+                .collect(toList());
+    }
+
+    public static int toInt(String str){
+        return Integer.parseInt(str);
+    }
+
+    public static long toLong(String str){
+        return Long.parseLong(str);
+    }
+
+}
